214e90f87bc4da554848fab9fe1d1aedcff82314
dipto0321 <diptokmk47@gmail.com>
2019-04-01 13:49:08 +0600

    Remove time and play with style

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,15 @@
 const app = document.getElementById("app");
 
-function tick() {
-  const time = new Date().toLocaleTimeString();
-  const element = <div>It is {time}</div>;
-  ReactDOM.render(element, app);
+function Box({className, style, ...rest}) {
+  return <div className={`box ${className}`} style={{padding: 20, ...style}} {...rest}/>
 }
-setInterval(tick, 1000);
+
+const boxElement = (
+  <div>
+    <Box className='box-small' style={{ backgroundColor: 'lightblue' }}>Small Box</Box>
+    <Box className='box-medium' style={{ backgroundColor: 'orange' }}>Medium Box</Box>
+    <Box className='box-large' style={{backgroundColor: 'pink'}}>Large Box</Box>
+  </div>
+);
+
+ReactDOM.render(boxElement, app);
