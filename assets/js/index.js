8d0a4345f6e68821eda6372016a41a50a222edc6
dipto0321 <diptokmk47@gmail.com>
2019-04-01 16:07:10 +0600

    Play with class components in react

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,21 @@
 const app = document.getElementById("app");
 
+class Counter extends React.Component{
+  constructor(...args) {
+    super(...args);
+  }
 
+  state = { count: 0 };
+  handleClick = ()=> {
+        this.setState(({ count }) => ({count: count + 1}));
+      }
+
+  render() {
+    return (
+      <button className="big-btn" onClick={this.handleClick}>{this.state.count}</button>
+    );
+  }
+}
+
+const element = <Counter/>
+ReactDOM.render(element, app);
